100e8cf2f91c083d492c48c8821002a0ebac0bee
pablopissoni <pablopissoni@gmail.com>
2023-04-23 04:45:29 -0300

    se agrego promises

---
+++ Server/src/controllers/getCharById.js
@@ -0,0 +1,32 @@
+// import axios from "axios"
+const axios = require("axios");
+
+const getCharById = (res, id) => {
+  axios(`https://rickandmortyapi.com/api/character/${id}`)
+    .then((response) => response.data)
+    .then(({ name, gender, species, image, origin, status }) => {
+      const character = {
+        id,
+        name,
+        gender,
+        species,
+        origin,
+        image,
+        status,
+      };
+
+      return res
+        .writeHead(200, { "Content-type": "application/json" })
+        .end(JSON.stringify(character));
+    })
+    .catch((error) => {
+      return res
+        .writeHead(500, { "Content-type": "text/plain" })
+        .end(error.message);
+    });
+};
+
+module.exports = {
+    getCharById,
+};
+
--- Server/src/index.js
+++ Server/src/index.js
@@ -1,22 +1,18 @@
 const http = require("http")
+const { getCharById } = require('./controllers/getCharById')
 // import http from require("http")
-const data = require("./utils/data")
+
+// const data = require("./utils/data")
 
 
 http.createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
 
-    const {url} = req
+    if(req.url.includes(`/rickandmorty/character`)){
+        const id = req.url.split('/').at(-1);
 
-    if (url.includes("rickandmorty/character/")) {
-        const id = url.split("/").at(-1)
-    const character = data.find((char) => char.id == id)
-    if (character) {
-        res.writeHead(200, {"Content-Type": "application/json"})
-        return res.end(JSON.stringify(character))
-    } else {
-        res.writeHead(400, {"Content-Type": "application/json"})
-        return res.end(JSON.stringify({error: "Character not found"}))
-        }
+
+        getCharById(res, +id);
     }
+
 }).listen(3001, "localhost")
